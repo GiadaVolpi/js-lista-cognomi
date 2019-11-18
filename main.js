e6f7b46033d53d8980d25d1dcdee89bb0f9ef74c
Gi@da <giadavolpi8@gmail.com>
2019-11-18 18:02:04 +0100

    creato array cognomi

---
+++ main.js
@@ -0,0 +1,9 @@
+// chiedere all'utente il cognome
+// inserire il cognome in un array
+// ordinare alfabeticamente
+// dire all'utente in che posizione si trova
+
+var lista = ["Acuto", "Bianco", "Cella", "Demichelis", "Einaudi", "Fabiani", "Garibaldi", "Iacuzzi", "Laguzzi", "Maccari", "Nannini", "Oberti", "Pirelli", "Quarto", "Rossi", "Sallustri", "Tasso", "Ubaldi", "Volpi", "Zaccagni"];
+
+var cognome = prompt ("Scrivi il tuo cognome");
+console.log (cognome);
